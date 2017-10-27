8849adb1e745c0d7e90d178052429a3dca9a698b
Drakirus <pierrescalade@gmail.com>
2017-10-27 15:04:16 +0200

    update date

--- config/config.exs
+++ config/config.exs
@@ -15,8 +15,8 @@ config :trash_duty, TrashDuty.Scheduler,
 
     # At 17:00 on Tuesday.
     {"0 17 * * 2",      {TrashDuty.Application, :notify_user_take_trash_out, []}},
-    # At 17:00 on Thursday.
-    {"0 17 * * 4",      {TrashDuty.Application, :notify_user_take_trash_out, []}},
+    # At 17:00 on Friday.
+    {"0 17 * * 5",      {TrashDuty.Application, :notify_user_take_trash_out, []}},
 
     # every 10 seconds // debug
     # {{:extended, "*/10 * * * *"}, {TrashDuty.Application, :notify_user_take_trash_out, []}},
